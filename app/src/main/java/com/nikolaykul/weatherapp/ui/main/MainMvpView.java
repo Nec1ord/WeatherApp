97df784aa0466a20a587098b98eb195534ee85d6
Nikolay <gN1kolaykul@gmail.com>
2016-11-16 01:03:49 +0300

    Fix: implement MvpView.

--- app/src/main/java/com/nikolaykul/weatherapp/ui/main/MainMvpView.java
+++ app/src/main/java/com/nikolaykul/weatherapp/ui/main/MainMvpView.java
@@ -2,12 +2,13 @@ package com.nikolaykul.weatherapp.ui.main;
 
 import com.google.android.gms.common.api.Status;
 import com.nikolaykul.weatherapp.data.model.forecast.Forecast;
+import com.nikolaykul.weatherapp.ui.base.view.MvpView;
 import com.nikolaykul.weatherapp.view.contract.CanShowError;
 import com.nikolaykul.weatherapp.view.contract.CanShowLoading;
 
 import java.util.List;
 
-public interface MainMvpView extends CanShowError, CanShowLoading {
+public interface MainMvpView extends CanShowError, CanShowLoading, MvpView {
 
     void showTodayForecast(List<Forecast> forecasts);
 
