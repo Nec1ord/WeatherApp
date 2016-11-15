61539e9e6bd0effe558c06bd7f52fd6f8fb49e05
Nikolay <gN1kolaykul@gmail.com>
2016-11-16 01:05:09 +0300

    Enable dagger injection.

--- app/src/main/java/com/nikolaykul/weatherapp/ui/search/SearchPresenter.java
+++ app/src/main/java/com/nikolaykul/weatherapp/ui/search/SearchPresenter.java
@@ -1,6 +1,14 @@
 package com.nikolaykul.weatherapp.ui.search;
 
+import com.nikolaykul.weatherapp.di.scope.PerActivity;
 import com.nikolaykul.weatherapp.ui.base.presenter.RxPresenter;
 
+import javax.inject.Inject;
+
+@PerActivity
 public class SearchPresenter extends RxPresenter<SearchMvpView> {
+
+    @Inject public SearchPresenter() {
+    }
+
 }
--- app/src/main/java/com/nikolaykul/weatherapp/ui/search/SuggestSearchView.java
+++ app/src/main/java/com/nikolaykul/weatherapp/ui/search/SuggestSearchView.java
@@ -6,14 +6,19 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.nikolaykul.weatherapp.R;
 import com.nikolaykul.weatherapp.databinding.SearchViewBinding;
+import com.nikolaykul.weatherapp.di.activity.ActivityComponent;
+import com.nikolaykul.weatherapp.view.contract.HasComponent;
+
+import javax.inject.Inject;
 
 import timber.log.Timber;
 
-public class SuggestSearchView extends LinearLayout implements SearchMvpView {
+public class SuggestSearchView extends RelativeLayout implements SearchMvpView {
+    @Inject protected SearchPresenter mPresenter;
     private SearchViewBinding mBinding;
 
     public SuggestSearchView(Context context) {
@@ -45,10 +50,27 @@ public class SuggestSearchView extends LinearLayout implements SearchMvpView {
         Timber.i("should show loading");
     }
 
+    @SuppressWarnings("unchecked")
     private void init() {
+        final ActivityComponent component = getActivityComponent();
+        if (null == component) {
+            Timber.e("Context doesn\'t has dagger component!");
+            return;
+        }
+        component.inject(this);
+
         mBinding = DataBindingUtil.inflate(
                 LayoutInflater.from(getContext()), R.layout.search_view, this, true);
+        // TODO: get component from activity & inject dependencies
+        // TODO: view model
+    }
 
+    @SuppressWarnings("unchecked")
+    private ActivityComponent getActivityComponent() {
+        final Context context = getContext();
+        return context instanceof HasComponent
+                ? ((HasComponent<ActivityComponent>) context).getComponent()
+                : null;
     }
 
 }
