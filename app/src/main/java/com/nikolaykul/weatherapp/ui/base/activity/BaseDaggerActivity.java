b4579f2ed62309b30a22459806eba04f333986a2
Nikolay <gN1kolaykul@gmail.com>
2016-11-16 00:05:24 +0300

    Create interface to get dagger component.

--- app/src/main/java/com/nikolaykul/weatherapp/ui/base/activity/BaseDaggerActivity.java
+++ app/src/main/java/com/nikolaykul/weatherapp/ui/base/activity/BaseDaggerActivity.java
@@ -8,14 +8,23 @@ import com.nikolaykul.weatherapp.WeatherApp;
 import com.nikolaykul.weatherapp.di.activity.ActivityComponent;
 import com.nikolaykul.weatherapp.di.activity.ActivityModule;
 import com.nikolaykul.weatherapp.di.activity.DaggerActivityComponent;
+import com.nikolaykul.weatherapp.view.contract.HasComponent;
 
-abstract class BaseDaggerActivity extends AppCompatActivity {
+abstract class BaseDaggerActivity extends AppCompatActivity
+        implements HasComponent<ActivityComponent> {
+
+    private ActivityComponent mComponent;
 
     protected abstract void injectSelf(ActivityComponent activityComponent);
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        injectSelf(createActivityComponent());
+        mComponent = createActivityComponent();
+        injectSelf(mComponent);
+    }
+
+    @Override public ActivityComponent getComponent() {
+        return mComponent;
     }
 
     private ActivityComponent createActivityComponent() {
---
+++ app/src/main/java/com/nikolaykul/weatherapp/view/contract/HasComponent.java
@@ -0,0 +1,5 @@
+package com.nikolaykul.weatherapp.view.contract;
+
+public interface HasComponent<T> {
+    T getComponent();
+}
